15830444b9ed0324558fd902d98eadd9a5c47920
Peter Walker <peterw@suprafax.net>
2013-04-26 13:13:00 -0400

    fixed example for multiple drives

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ var freedisk = require("./freedisk.js");
 
 freedisk.drivelist(function(error,drives){
 
-	if (error !== null) {
-	      console.log(error);
+    if (error !== null) {
+          console.log(error);
     }
     else {
 
-    	for (var i = 0; i < drives.length; i++) {
-    		var mydrive = drives[i];
-    		
-    		freedisk.detail(mydrive,function(error,total,used,free){
+        for (var i = 0; i < drives.length; i++) {
 
-    			if (error !== null) {
-    				console.log(error);
-    			}
-    			else {
-    				//total != used+free because it's using df -h
-    				console.log("Drive : " + mydrive);
-    				console.log("Total : " + total);
-    				console.log("Used : " + used);
-    				console.log("Free : " + free);
-    			}
+            (function (mydrive) {
 
-    		});//end of detail
-    	}
+                freedisk.detail(mydrive,function(error,total,used,free){
+
+                    if (error !== null) {
+                        console.log(error);
+                    }
+                    else {
+                        //total != used+free because it's using df -h
+                        console.log("Drive : " + mydrive);
+                        console.log("Total : " + total);
+                        console.log("Used : " + used);
+                        console.log("Free : " + free);
+                    }
+
+                });//end of detail
+
+            }(drives[i])); // end closure
+        }
     }
 });
